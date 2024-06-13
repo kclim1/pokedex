c8627136bcde378eb74aee1dca66e094bbf66d88
KC Lim <kclim0528@gmail.com>
2024-06-13 01:03:15 -0700

    added html boilerplates and changed background color to a linear gradient of dark blue to light blue

--- server/app.js
+++ server/app.js
@@ -1,15 +1,19 @@
 const express = require("express");
 const axios = require("axios");
-const port = 5000;
+const port = 3000;
 const app = express()
 const cors=require('cors')
-let name = 
+// let name = 
 
 async function fetchPokemon(){
     try{
-        const pokeData = await axios.get(`https://pokeapi.co/api/v2/${name}`)
-        // const data = pokeData.data[0]
-        console.log(pokeData)
+        const pokeData = await axios.get(`https://pokeapi.co/api/v2/pokemon/bulbasaur/`)
+        const data = pokeData.data
+        // console.log(data.name)
+        console.log(data.order)
+        console.log(data.species.name)
+        console.log(data.sprites.versions["generation-v"]["black-white"].animated.front_default);
+        console.log(data.stats[3].base_stat)
     }
     catch(error){
         console.error(error)
@@ -17,5 +21,6 @@ async function fetchPokemon(){
 }
 fetchPokemon()
 
-
-// console.log('hello')
+app.listen(3000,()=>{
+    console.log(`app running on port ${port}` )
+})
